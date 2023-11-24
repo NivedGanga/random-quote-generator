741049c7d31dddb87502a0e1d758d078d8c5890a
Nived G <106334224+NivedGanga@users.noreply.github.com>
2023-11-24 20:59:41 +0530

    Add files via upload
    files added

---
+++ js/webcraft.js
@@ -0,0 +1,39 @@
+// javascript
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+
+
+
+// variable
+
+// let
+// const
+// datatypes
+
+// const fruits=["apple","banana","mango","orange","grapes"]
+// console.log(fruits[2
+// ])
+
+
+// const person = {
+//     name: "John",
+//     age: 30,
+// }
+// console.log(person.name)
+
+
+// functions
+// function add() {
+    
+// }
+
+
+
+// math
+// random
+ 
+
+
